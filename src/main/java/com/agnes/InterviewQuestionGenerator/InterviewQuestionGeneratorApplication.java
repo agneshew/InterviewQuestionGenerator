2b0781d81dbd6c5d6f5e645f48514ce3b99cd45c
agneshew <agnes.hewusz@gmail.com>
2021-05-21 08:59:15 +0200

    Initial commit

---
+++ src/main/java/com/agnes/InterviewQuestionGenerator/InterviewQuestionGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.agnes.InterviewQuestionGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewQuestionGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewQuestionGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agnes/InterviewQuestionGenerator/InterviewQuestionGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agnes.InterviewQuestionGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewQuestionGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
